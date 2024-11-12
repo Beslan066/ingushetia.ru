fb3d4ac01e52a6006a1c2bb4dd0972aa8bcc9d93
windstep <icq431@yandex.ru>
2024-11-12 05:28:16 +0300

    fix: style tweaks

--- resources/frontend/molecules/accordion/Accordion.jsx
+++ resources/frontend/molecules/accordion/Accordion.jsx
@@ -1,16 +1,25 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import './accordion.css'
 
 export default function Accordion({ title, children }) {
   const [open, setOpen] = useState(false)
+  const drawerRef = useRef()
+
+  const handle = () => {
+    setOpen(!open)
+    drawerRef.current.style.maxHeight = !open ? `${drawerRef.current.scrollHeight}px` : '0';
+  }
+
   return (
     <div className={ `accordion accordion--${open ? 'opened' : 'closed'}` }>
-      <button className="accordion__header" onClick={ () => setOpen(!open) }>
+      <button className="accordion__header" onClick={ handle }>
         <span>{ title }</span>
         <span className="accordion__icon"><img src={ open ? '/img/icons/Minus.svg' : '/img/icons/Plus.svg' } alt="иконка открытия"/></span>
       </button>
-      <div className="accordion__body">
-        { children }
+      <div className="accordion__body" ref={drawerRef}>
+        <div className="accordion-body__content">
+          { children }
+        </div>
       </div>
     </div>
   )
